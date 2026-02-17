29fd68e360d18983f2941abb9778284c63fdf91b
LeonardoAhh <capacitacionqrovino@gmail.com>
2026-02-17 07:50:15 -0600

    update and fix

--- src/app/capacitacion/page.js
+++ src/app/capacitacion/page.js
@@ -127,43 +127,7 @@ export default function CapacitacionPage() {
             ),
             color: 'purple'
         },
-        {
-            href: '/capacitacion/habilidades',
-            title: 'Matriz de Habilidades',
-            description: 'Competencias por área',
-            icon: (
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                    <path d="M9 11l3 3L22 4" />
-                    <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11" />
-                </svg>
-            ),
-            color: 'green'
-        },
-        {
-            href: '/capacitacion/cursos',
-            title: 'Vigencia de Cursos',
-            description: 'Control de renovaciones',
-            icon: (
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                    <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" />
-                    <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z" />
-                </svg>
-            ),
-            color: 'orange'
-        },
-        {
-            href: '/capacitacion/alertas',
-            title: 'Alertas Inteligentes',
-            description: 'Notificaciones automáticas',
-            icon: (
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                    <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z" />
-                    <line x1="12" y1="9" x2="12" y2="13" />
-                    <line x1="12" y1="17" x2="12.01" y2="17" />
-                </svg>
-            ),
-            color: 'red'
-        },
+
         {
             href: '/capacitacion/calendario',
             title: 'Calendario',
